d1f10b23443b3c5b6effb062b556518b2bc2eafe
Adam Auckland <adamauckland@gmail.com>
2016-10-09 20:50:44 +0100

    default published date

---
+++ client/README.md
@@ -0,0 +1,5 @@
+Useful looking plugins:
+
+http://www.plugreg.com/plugin/cv-library/phonegap-plugin-dropbox-chooser
+
+https://github.com/cv-library/phonegap-plugin-dropbox-chooser
--- client/www/js/controllers/news-detail.controller.js
+++ client/www/js/controllers/news-detail.controller.js
@@ -1,22 +1,35 @@
 angular
 	.module('controllers.news-detail.controller', [])
-	.controller('NewsDetailController', function($scope, $stateParams, News, $ionicNavBarDelegate) {
-		$scope.article = News.get($stateParams.id);
+	.controller('NewsDetailController', [
+		'$scope',
+		'$stateParams',
+		'News',
+		'$ionicNavBarDelegate',
+		'$window',
+		function($scope, $stateParams, News, $ionicNavBarDelegate, $window) {
+			$scope.article = News.get($stateParams.id);
 
-		$scope.newsSaveClick = function () {
-			var data = $scope.article;
-			data.date_published = moment(data.date_published).format('YYYY-MM-DD');
+			$scope.onPublishedChange = function () {
+				if ($scope.article.published === true) {
+					$scope.article.date_published = new Date();
+				}
+			};
 
-			if (data.date_published === 'Invalid date') {
-				delete data.date_published;
-			}
+			$scope.newsSaveClick = function () {
+				var data = $scope.article;
+				data.date_published = $window.moment(data.date_published).format('YYYY-MM-DD');
 
-			News
-				.save(data)
-				.then(function () {
-					$ionicNavBarDelegate.back();
-				}, function () {
+				if (data.date_published === 'Invalid date') {
+					delete data.date_published;
+				}
 
-				});
-		};
-	});
+				News
+					.save(data)
+					.then(function () {
+						$ionicNavBarDelegate.back();
+					}, function () {
+
+					});
+			};
+		}
+	]);
